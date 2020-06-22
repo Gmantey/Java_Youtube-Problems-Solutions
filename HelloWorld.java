1a7239f90bd695fe473a51fc29f98e578b19607e
Mantey <63980989+Gmantey@users.noreply.github.com>
2020-06-22 17:11:01 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+Prompt:
+    Write a program that prints out
+    "hello world" to the terminal Screen
+
+Name:
+Date:
+ */
+
+
+/*
+Solution
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Subscribe for more!!!! Thank you so much");
+
+    }
+
+}
